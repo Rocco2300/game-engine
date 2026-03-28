294938baee5a598b8bdff1100b6febc2ba9c1125
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2026-03-28 22:47:26 +0200

    we have some sort of frustum culling which doesn't work too well

--- include/scene_renderer.hpp
+++ include/scene_renderer.hpp
@@ -7,6 +7,21 @@
 #include "renderer.hpp"
 #include "fps_camera.hpp"
 
+struct Plane {
+    glm::vec3 position{};
+    glm::vec3 normal{};
+};
+
+struct Frustum {
+    Plane nearPlane{};
+    Plane farPlane{};
+
+    Plane topPlane{};
+    Plane bottomPlane{};
+    Plane leftPlane{};
+    Plane rightPlane{};
+};
+
 class SceneRenderer : public IRenderer {
 public:
     SceneRenderer();
--- sources/scene_renderer.cpp
+++ sources/scene_renderer.cpp
@@ -1,9 +1,57 @@
 #include "scene_renderer.hpp"
 
-#include "global.hpp"
 #include "asset_manager.hpp"
+#include "global.hpp"
 
 #include <GL/gl3w.h>
+
+static Frustum buildFrustum(const FPSCamera& camera) {
+    Frustum frustum{};
+
+    const float zFar  = 100.f;
+    const float zNear = 0.1f;
+
+    const float halfVSide        = zFar * tanf(camera.fov() * 0.5f);
+    const float halfHSide        = halfVSide * camera.aspect();
+    const glm::vec3 frontMultFar = zFar * camera.front();
+
+    // clang-format off
+    frustum.nearPlane = { camera.position() + zNear * camera.front(), camera.front() };
+    frustum.farPlane = { camera.position() + frontMultFar, -camera.front() };
+    frustum.topPlane = { camera.position(), glm::normalize(glm::cross(frontMultFar - camera.up() * halfVSide, camera.right())) };
+    frustum.bottomPlane = { camera.position(), glm::normalize(glm::cross(camera.right(), frontMultFar + camera.up() * halfVSide)) };
+    frustum.rightPlane = { camera.position(), glm::normalize(glm::cross(frontMultFar + camera.right() * halfHSide, camera.up())) };
+    frustum.leftPlane = { camera.position(), glm::normalize(glm::cross(camera.up(), frontMultFar - camera.right() * halfHSide)) };
+    // clang-format on
+
+    return frustum;
+}
+
+static float getSignedDistance(const glm::vec3& point, const Plane& plane) {
+    return glm::dot(plane.normal, point - plane.position);
+}
+
+static bool isInFront(const glm::vec3& position, const AABB& aabb, const Plane& plane) {
+    glm::vec3 extents{};
+    extents.x = aabb.x / 2.f;
+    extents.y = aabb.y / 2.f;
+    extents.z = aabb.z / 2.f;
+
+    const float r = extents.x * std::abs(plane.normal.x) + extents.y * std::abs(plane.normal.y) +
+                    extents.z * std::abs(plane.normal.z);
+    const float signedDistance = getSignedDistance(position, plane);
+
+    return -r <= signedDistance;
+}
+
+static bool isInFrustum(const glm::vec3& position, const AABB& aabb, const Frustum& frustum) {
+    return isInFront(position, aabb, frustum.topPlane) &&
+           isInFront(position, aabb, frustum.bottomPlane) &&
+           isInFront(position, aabb, frustum.leftPlane) &&
+           isInFront(position, aabb, frustum.rightPlane) &&
+           isInFront(position, aabb, frustum.farPlane) &&
+           isInFront(position, aabb, frustum.nearPlane);
+}
 
 SceneRenderer::SceneRenderer()
     : IRenderer(global.shadersPath, "scene") {}
@@ -27,6 +75,11 @@ void SceneRenderer::draw(const IDrawable& drawable) const {
     // TODO: this might throw, in general shouldn't if everything is correctly set up
     const auto& scene = dynamic_cast<const Scene&>(drawable);
 
+    Frustum frustum;
+    if (m_camera) {
+        frustum = buildFrustum(*m_camera);
+    }
+
     m_program.use();
 
     m_program.setUniformLight(*m_light);
@@ -39,7 +92,11 @@ void SceneRenderer::draw(const IDrawable& drawable) const {
 
         auto* model = AssetManager::getModel(entity.modelId);
         if (model) {
-            drawModelImpl(*model);
+            if (isInFrustum(entity.position, model->aabb(), frustum)) {
+                drawModelImpl(*model);
+            } else {
+                std::cout << "Out of frustum!\n";
+            }
         }
     }
 }
